8ccf1854db5bd31ac4386c187180bd96cf51cb29
kazouke <kazouke@hotmail.fr>
2017-01-08 18:15:31 +0100

    Ajout de tests

--- test/TestPile.cpp
+++ test/TestPile.cpp
@@ -3,12 +3,11 @@
 #include "../src/Pile.h"
 #include "catch.hpp"
 
-
 void testAjoutValeur(const double &val,Pile &p)
 {
-	int nouvelleTaillePile = p.size();
+	int nouvelleTaillePile = p.size() +1;
 	p.push(val);
-	nouvelleTaillePile += 1;
+	
 	REQUIRE(p.size() == nouvelleTaillePile);
 	REQUIRE(p.peek() == val);
 }
@@ -20,19 +19,22 @@ bool pileEstVide(Pile &p)
 
 TEST_CASE("Tests sur une pile", "[pile]")
 {
-	double valeurInitiale = 0.0;
 	double premiereValeurPile = 10.5;
 	double deuxiemeValeurPile = 5.3;
 	double troisiemeValeurPile = 20.1;
+	double quatriemeValeurPile = 42.2;
+	double cinquiemeValeurPile = 18.4;
+	double sixiemeValeurPile = 22.8;
 	Pile p{};
 	
-	SECTION("Pile par défaut correctement créée")
+	SECTION("Pile par defaut correctement creee")
 	{
+		double valeurInitiale = 0.0;
 		Pile p1{};
 		REQUIRE(p1.peek()==valeurInitiale);
 		REQUIRE(p1.size()==1);
 	}
-	SECTION("Pile avec paramètre correctement créée")
+	SECTION("Pile avec parametre correctement creee")
 	{
 		Pile p1{premiereValeurPile};
 		REQUIRE(p1.peek()==premiereValeurPile);
@@ -41,7 +43,7 @@ TEST_CASE("Tests sur une pile", "[pile]")
 	
 	p.push(premiereValeurPile);
 	
-	SECTION("Une pile vide est bien vide")
+	SECTION("Une pile vide est bien vide et se vide correctement")
 	{		
 		testAjoutValeur(deuxiemeValeurPile,p);
 		testAjoutValeur(troisiemeValeurPile,p);
@@ -62,19 +64,60 @@ TEST_CASE("Tests sur une pile", "[pile]")
 		REQUIRE(pileEstVide(p));
 	}
 	
-	SECTION("Une valeur est correctement ajoutée à la pile")
+	SECTION("Une valeur est correctement ajoutee a la pile")
 	{	 
 		testAjoutValeur(deuxiemeValeurPile,p);
 		testAjoutValeur(troisiemeValeurPile,p);		
 	}
 	
-	SECTION("Une valeur est correctement retirée à la pile")
+	SECTION("Une valeur est correctement retiree a la pile")
 	{
 		
+		p.push(deuxiemeValeurPile);
+		p.push(troisiemeValeurPile);
+		double nouvelleDerniereValeur = deuxiemeValeurPile;
+		
+		int nouvelleTaillePile = p.size()-1;
+		p.pop_back();
+		double derniereValeurPile = p.peek();
+		
+		REQUIRE(derniereValeurPile == nouvelleDerniereValeur);
+		REQUIRE(p.size() == nouvelleTaillePile);
 	}
 	
+	SECTION("Une valeur retiree renvoie correctement sa valeur")
+	{
+		p.push(deuxiemeValeurPile);
+		p.push(troisiemeValeurPile);
+		
+		int nouvelleTaillePile = p.size()-1;
+		double valeurASupprimer = p.peek();
+		double valeurSupprimee = p.return_pop_back();
+		
+		REQUIRE(valeurASupprimer == valeurSupprimee);
+		REQUIRE(p.size() == nouvelleTaillePile);
+				
+	}
 	
+	SECTION("Deux valeurs s'echangent correctement")
+	{
+		p.push(deuxiemeValeurPile);
+		p.push(troisiemeValeurPile);
+		
+		p.swap();
+		REQUIRE(p.return_pop_back() == deuxiemeValeurPile);
+		REQUIRE(p.return_pop_back() == troisiemeValeurPile);
+	}
 	
-	
-	
+	SECTION("Les 5 dernieres valeurs sont renvoyees correctement")
+	{
+		p.push(deuxiemeValeurPile);
+		p.push(troisiemeValeurPile);
+		
+		//
+		p.push(quatriemeValeurPile);
+		p.push(cinquiemeValeurPile);
+		p.push(sixiemeValeurPile);
+		
+	}
 }
